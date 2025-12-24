df27e294f2098f860ffa304b59b426419a91486e
CTOSoficial <pezoaa23@gmail.com>
2025-12-24 18:26:41 -0300

    Add files via upload

---
+++ Plan B/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir archivos estáticos de la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Ruta de ayuda
+app.get('/help', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/help.html'));
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
